81573f42d7f227c9ccf0662887b6b4421b71b07e
kenanboyukkisiyev <git config --global user.emailkanannb@code.edu.az>
2017-09-27 16:48:54 +0400

    ev tapsirigi kenana papkasi

---
+++ tempilate.js
@@ -0,0 +1,9 @@
+ $(".text").mouseover(function(){
+        $(".text").css("background-color", "white",
+        	             "color","red" 
+        	                  );
+    });
+ $(".text").mouseout(function(){
+        $(".text").css('background-color', 'blueviolet','color','white');
+    });
+ 
