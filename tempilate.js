f0d1231a39b19b502d10ae534ac80ba822f26dd5
kenanboyukkisiyev <git config --global user.emailkanannb@code.edu.az>
2017-09-29 18:55:36 +0400

    s

--- tempilate.js
+++
@@ -1,9 +0,0 @@
- $(".text").mouseover(function(){
-        $(".text").css("background-color", "white",
-        	             "color","red" 
-        	                  );
-    });
- $(".text").mouseout(function(){
-        $(".text").css('background-color', 'blueviolet','color','white');
-    });
- 
